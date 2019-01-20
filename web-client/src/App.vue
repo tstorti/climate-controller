ebb547a62802d023a5372016009197377f781826
Tony Storti <tonystorti@gmail.com>
2019-01-20 12:06:38 -0600

    added firebase config

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,13 +1,11 @@
-
-
 <script>
-import Home from './components/Home.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'app',
   components: {
     home: Home
-  }
+  },
 }
 </script>
 
@@ -22,8 +20,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- web-client/src/main.js
+++ web-client/src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import config from './../config';
+import * as firebase from 'firebase';
+
+const firebaseConfig = config.firebase;
+
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore through Firebase
+//const db = firebase.firestore();
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
