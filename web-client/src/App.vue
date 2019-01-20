6cdd3c4abd30a013347ba065cd34d103296a37c7
Tony Storti <tonystorti@gmail.com>
2019-01-20 10:22:47 -0600

    initalize app with vue cli

---
+++ web-client/src/App.vue
@@ -0,0 +1,29 @@
+
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    home: Home
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
